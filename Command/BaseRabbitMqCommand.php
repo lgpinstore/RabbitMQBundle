ed3e6b1ff4ad9473a95fdd09b157cbed7579cebd
agriboed <alexv1rs@gmail.com>
2024-07-23 13:25:03 +0200

    Change constructor.

--- Command/BaseRabbitMqCommand.php
+++ Command/BaseRabbitMqCommand.php
@@ -7,13 +7,6 @@ use Symfony\Component\Console\Command\Command;
 
 abstract class BaseRabbitMqCommand extends Command
 {
-    public function __construct(
-        protected ContainerInterface $container,
-    )
-    {
-        parent::__construct();
-    }
-
     public function getContainer(): ContainerInterface
     {
         return $this->container;
