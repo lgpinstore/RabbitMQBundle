d1d2dd90bac8426aa21ea9b69b12d4ce45b33e0a
agriboed <alexv1rs@gmail.com>
2024-07-23 13:57:49 +0200

    Dependency injection in constructor.

--- Command/BaseRabbitMqCommand.php
+++ Command/BaseRabbitMqCommand.php
@@ -7,6 +7,14 @@ use Symfony\Component\Console\Command\Command;
 
 abstract class BaseRabbitMqCommand extends Command
 {
+    protected ?ContainerInterface $container;
+
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+
+        parent::__construct();
+    }
+
     public function getContainer(): ContainerInterface
     {
         return $this->container;
